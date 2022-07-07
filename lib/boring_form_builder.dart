3878ff922a8dea9aca5acb279272c247706711de
handgull <gullandrea693@gmail.com>
2022-07-07 16:41:26 +0200

    boring field exported

--- lib/boring_form_builder.dart
+++ lib/boring_form_builder.dart
@@ -2,6 +2,7 @@ library boring_form_builder;
 
 export 'src/form/boring_form.dart';
 export 'src/sections/boring_section.dart';
+export 'src/fields/boring_field.dart';
 export 'src/form/boring_form_controller.dart';
 export 'src/fields/boring_field_controller.dart';
 export 'src/fields/implementations/boring_text_field.dart';
